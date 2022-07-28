2b38cad16a0eeeff6e3fa29c5d2018d6843d97b6
Swapnil Kulkarni <meswapnilkulkarni@gmail.com>
2022-07-28 10:25:54 +0530

    Variable Declaration

---
+++ 002.Variables/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var card string = "Ace of Spades" //statically typed variable declaration
+	card2 := "Ace of Spades"          // dynamically typed variable declaration
+	fmt.Println(card)
+	fmt.Println(card2)
+}
